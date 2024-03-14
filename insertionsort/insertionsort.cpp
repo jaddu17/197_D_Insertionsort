275503dddae45caf5333d7cac10851d8f7e726b9
jaddu17 <shafajaddu@gmail.com>
2024-03-14 10:26:13 +0700

    Membuat prosedur untuk menampilkan hasil

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -4,7 +4,31 @@ using namespace std;
 int arr[20];                        // Membuat Array dengan panjang data 20
 int n;                              // Membuat variabel inputan n
 
+void input() {                      // Produce Input
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";       // Membuat Inputan jumlah element Array
+        cin >> n;                                           // Memanggil Variabel inputan n
+
+        if (n <= 20) {                                      // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";     // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+    cout << endl;                                   // Membuat jarak per baris program
+    cout << "======================" << endl;       // Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
 
 
+    for (int i = 0; i < n; i++)                     // Menggunakan perulangan for untuk menyimpan data pada Array
+    {
+        cout << "Data ke-" << (i + 1) << ":";       // Memasukkkan atau menginputkan nilai data n
+        cin >> arr[i];                              // Menyimpan nilai data n kedalam array arr
+    }
+}
 
 
