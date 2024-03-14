629bcd10ced13333cfa79abe0bbec32b231b0038
jaddu17 <shafajaddu@gmail.com>
2024-03-14 10:10:00 +0700

    Add project files.

---
+++ insertionsort/insertionsort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];                        // Membuat Array dengan panjang data 20
+int n;                              // Membuat variabel inputan n
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
